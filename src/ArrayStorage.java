ee9e68f029a82e729904eec551c2b475472c6711
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-03 15:26:24 +0300

    HW0 v.1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,63 +3,54 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int sizeStorage = 0;
 
     void clear() {
-        for(int i=0; i< size(); i++)
+        for (int i = 0; i < size(); i++)
             storage[i] = null;
+        sizeStorage = 0;
     }
 
     void save(Resume r) {
-        if (size()< storage.length)
-            storage[size()]=r;
-        else
+        if (size() < storage.length) {
+            storage[size()] = r;
+            sizeStorage++;
+        } else
             System.out.println("Хранилище заполнено полностью. Сохранить элемент не возможно.");
     }
 
     Resume get(String uuid) {
-        for(Resume r : getAll()) {
-            if (r.uuid.equals(uuid)) {
-                return r;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int index = -1;
-        for(int i=0; i<size(); i++){
-            if (storage[i].uuid.equals(uuid)){
-                index = i;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size() - 1];
                 break;
             }
         }
-        int sizeStorage = size()-1;
-        if (index > -1) {
-            for (int i=index; i<sizeStorage; i++)
-                storage[i] = storage[i+1];
-        }
-        storage[sizeStorage] = null;
+        storage[size() - 1] = null;
+        sizeStorage--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] result = new Resume[size()];
+        Resume[] allResume = new Resume[size()];
         if (size() >= 0) {
-            System.arraycopy(storage, 0, result, 0, size());
+            System.arraycopy(storage, 0, allResume, 0, size());
         }
-        return result;
+        return allResume;
     }
 
     int size() {
-        int result = 0;
-        for (Resume resume : storage)
-            if (resume != null) {
-                result++;
-            } else {
-                break;
-            }
-        return result;
+        return sizeStorage;
     }
 }
