7d1f03993c0283a301b8f18f9eded481f4d01ce5
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-03 13:32:15 +0300

    HW0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,61 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for(int i=0; i< size(); i++)
+            storage[i] = null;
     }
 
     void save(Resume r) {
+        if (size()< storage.length)
+            storage[size()]=r;
+        else
+            System.out.println("Хранилище заполнено полностью. Сохранить элемент не возможно.");
     }
 
     Resume get(String uuid) {
+        for(Resume r : getAll()) {
+            if (r.uuid.equals(uuid)) {
+                return r;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = -1;
+        for(int i=0; i<size(); i++){
+            if (storage[i].uuid.equals(uuid)){
+                index = i;
+                break;
+            }
+        }
+        int sizeStorage = size()-1;
+        if (index > -1) {
+            for (int i=index; i<sizeStorage; i++)
+                storage[i] = storage[i+1];
+        }
+        storage[sizeStorage] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size()];
+        if (size() >= 0) {
+            System.arraycopy(storage, 0, result, 0, size());
+        }
+        return result;
     }
 
     int size() {
-        return 0;
+        int result = 0;
+        for (Resume resume : storage)
+            if (resume != null) {
+                result++;
+            } else {
+                break;
+            }
+        return result;
     }
 }
