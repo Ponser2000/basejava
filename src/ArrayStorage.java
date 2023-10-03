f1448e85d4b0c6b51fd577a924c6205212c44d76
Ponser2000 <ponser2000@gmail.com>
2023-10-03 19:07:26 +0300

    HW0 rev.3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,58 +2,59 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int sizeStorage = 0;
 
-    void clear() {
-        for (int i = 0; i < size(); i++)
-            storage[i] = null;
-        sizeStorage = 0;
+  Resume[] storage = new Resume[10000];
+  int sizeStorage;
+
+  void clear() {
+    for (int i = 0; i < sizeStorage; i++) {
+      storage[i] = null;
     }
+    sizeStorage = 0;
+  }
 
-    void save(Resume r) {
-        if (size() < storage.length) {
-            storage[size()] = r;
-            sizeStorage++;
-        } else
-            System.out.println("Хранилище заполнено полностью. Сохранить элемент не возможно.");
+  void save(Resume r) {
+    if (sizeStorage < storage.length) {
+      storage[sizeStorage] = r;
+      sizeStorage++;
+    } else {
+      System.out.println("Хранилище заполнено полностью. Сохранить элемент не возможно.");
     }
+  }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
-        }
-        return null;
+  Resume get(String uuid) {
+    for (int i = 0; i < sizeStorage; i++) {
+      if (storage[i].uuid.equals(uuid)) {
+        return storage[i];
+      }
     }
+    return null;
+  }
 
-    void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size() - 1];
-                break;
-            }
-        }
-        storage[size() - 1] = null;
+  void delete(String uuid) {
+    for (int i = 0; i < sizeStorage; i++) {
+      if (storage[i].uuid.equals(uuid)) {
+        storage[i] = storage[sizeStorage - 1];
+        storage[sizeStorage - 1] = null;
         sizeStorage--;
+        break;
+      }
     }
+  }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        Resume[] allResume = new Resume[size()];
-        if (size() >= 0) {
-            for (int i = 0; i < size(); i++) {
-                allResume[i] = new Resume();
-                allResume[i].uuid = storage[i].uuid;
-            }
-        }
-        return allResume;
+  /**
+   * @return array, contains only Resumes in storage (without null)
+   */
+  Resume[] getAll() {
+    Resume[] allResume = new Resume[sizeStorage];
+    for (int i = 0; i < sizeStorage; i++) {
+      allResume[i] = new Resume();
+      allResume[i].uuid = storage[i].uuid;
     }
+    return allResume;
+  }
 
-    int size() {
-        return sizeStorage;
-    }
+  int size() {
+    return sizeStorage;
+  }
 }
