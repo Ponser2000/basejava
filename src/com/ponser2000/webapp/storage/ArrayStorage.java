134962a33ce07ad6d44ea0c539dc92df716ea6c4
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-04 19:25:51 +0300

    HW2 v.1

---
+++ src/com/ponser2000/webapp/storage/ArrayStorage.java
@@ -0,0 +1,82 @@
+package com.ponser2000.webapp.storage;
+/**
+ * Array based storage for Resumes
+ */
+
+import com.ponser2000.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class ArrayStorage {
+
+    private Resume[] storage = new Resume[10000];
+    private int sizeStorage;
+
+    public void clear() {
+        Arrays.fill(storage, 0, sizeStorage, null);
+        sizeStorage = 0;
+    }
+
+    public void update(Resume r) {
+        int indexResume = getIndexResume(r.getUuid());
+        if (indexResume >= 0) {
+                storage[indexResume] = r;
+            } else {
+            System.out.println("Обновить элемент не возможно. Резуме UUID: " + r.getUuid() + " отсутсвует в базе");
+        }
+    }
+
+    public void save(Resume r) {
+        if (sizeStorage >= storage.length) {
+            System.out.println("Хранилище заполнено полностью. Сохранить резуме UUID: " + r.getUuid() +  "не возможно.");
+        } else if (getIndexResume(r.getUuid()) < 0) {
+            storage[sizeStorage] = r;
+            sizeStorage++;
+        } else {
+            System.out.println("Сохранить элемент не возможно. Резуме UUID: " + r.getUuid() + " уже есть в базе");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int indexResume = getIndexResume(uuid);
+        if (indexResume >= 0) {
+            return storage[indexResume];
+        } else {
+            System.out.println("Резуме UUID: " + uuid + " отсутсвует в базе");
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        int indexResume = getIndexResume(uuid);
+        if (indexResume >= 0) {
+            storage[indexResume] = storage[sizeStorage - 1];
+            storage[sizeStorage - 1] = null;
+            sizeStorage--;
+        } else {
+            System.out.println("Резуме UUID: " + uuid + " отсутсвует в базе");
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, sizeStorage);
+    }
+
+    public int size() {
+        return sizeStorage;
+    }
+
+    public int getIndexResume(String uuid) {
+        int index = -1;
+        for (int i = 0; i < sizeStorage; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
