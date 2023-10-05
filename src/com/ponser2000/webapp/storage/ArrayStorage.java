0d39e1c345bca6b3e01fbd0f2b9efea7324210de
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-05 19:47:43 +0300

    HW2 v.3 (final)

--- src/com/ponser2000/webapp/storage/ArrayStorage.java
+++ src/com/ponser2000/webapp/storage/ArrayStorage.java
@@ -9,28 +9,29 @@ import java.util.Arrays;
 
 public class ArrayStorage {
 
-    private Resume[] storage = new Resume[10000];
+    private final int STORAGE_LIMIT = 10000;
+    private final Resume[] STORAGE = new Resume[STORAGE_LIMIT];
     private int sizeStorage;
 
     public void clear() {
-        Arrays.fill(storage, 0, sizeStorage, null);
+        Arrays.fill(STORAGE, 0, sizeStorage, null);
         sizeStorage = 0;
     }
 
     public void update(Resume r) {
-        int indexResume = getIndexResume(r.getUuid());
-        if (indexResume >= 0) {
-                storage[indexResume] = r;
-            } else {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            STORAGE[index] = r;
+        } else {
             System.out.println("Обновить элемент не возможно. Резюме UUID: " + r.getUuid() + " отсутствует в базе");
         }
     }
 
     public void save(Resume r) {
-        if (sizeStorage >= storage.length) {
-            System.out.println("Хранилище заполнено полностью. Сохранить резюме UUID: " + r.getUuid() +  "не возможно.");
-        } else if (getIndexResume(r.getUuid()) < 0) {
-            storage[sizeStorage] = r;
+        if (sizeStorage >= STORAGE.length) {
+            System.out.println("Хранилище заполнено полностью. Сохранить резюме UUID: " + r.getUuid() + "не возможно.");
+        } else if (getIndex(r.getUuid()) < 0) {
+            STORAGE[sizeStorage] = r;
             sizeStorage++;
         } else {
             System.out.println("Сохранить элемент не возможно. Резюме UUID: " + r.getUuid() + " уже есть в базе");
@@ -38,9 +39,9 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int indexResume = getIndexResume(uuid);
-        if (indexResume >= 0) {
-            return storage[indexResume];
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            return STORAGE[index];
         } else {
             System.out.println("Резюме UUID: " + uuid + " отсутствует в базе");
         }
@@ -48,10 +49,10 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int indexResume = getIndexResume(uuid);
-        if (indexResume >= 0) {
-            storage[indexResume] = storage[sizeStorage - 1];
-            storage[sizeStorage - 1] = null;
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            STORAGE[index] = STORAGE[sizeStorage - 1];
+            STORAGE[sizeStorage - 1] = null;
             sizeStorage--;
         } else {
             System.out.println("Резюме UUID: " + uuid + " отсутствует в базе");
@@ -62,17 +63,17 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, sizeStorage);
+        return Arrays.copyOf(STORAGE, sizeStorage);
     }
 
     public int size() {
         return sizeStorage;
     }
 
-    public int getIndexResume(String uuid) {
+    public int getIndex(String uuid) {
         int index = -1;
         for (int i = 0; i < sizeStorage; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (STORAGE[i].getUuid().equals(uuid)) {
                 index = i;
                 break;
             }
