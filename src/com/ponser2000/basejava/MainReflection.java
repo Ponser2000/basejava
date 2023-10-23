2a3ff0070947f7551a1b6a37b6b9af20c4e02f52
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-23 12:45:41 +0300

    HW4 invoke r.toString via reflection

--- src/com/ponser2000/basejava/MainReflection.java
+++ src/com/ponser2000/basejava/MainReflection.java
@@ -4,16 +4,21 @@ import com.ponser2000.basejava.model.Resume;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+
         // TODO : invoke r.toString via reflection
+        Method method = Class.forName(r.getClass().getName()).getMethod("toString");
+        System.out.println(method.invoke(r));
+
         System.out.println(r);
     }
 }
