f7219f8bba1fb2997aa687f4f48049f562d28926
Sergey Ponomarev <ponser2000@gmail.com>
2023-10-31 14:06:39 +0300

    HW6  - add class MapResumeStorage and JUnit test

---
+++ src/com/ponser2000/basejava/storage/MapResumeStorage.java
@@ -0,0 +1,59 @@
+package com.ponser2000.basejava.storage;
+
+import com.ponser2000.basejava.model.Resume;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapResumeStorage extends AbstractStorage {
+    protected final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected int doSize() {
+        return storage.size();
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get(((Resume) searchKey).getUuid());
+    }
+
+    @Override
+    protected List<Resume> doGetAll() {
+        return storage.values().stream()
+                .sorted(FULLNAME_COMPARATOR.thenComparing(UUID_COMPARATOR))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected Object getSearchKey(String searchKey) {
+        return storage.get(searchKey);
+    }
+
+    @Override
+    protected void doUpdate(Object searchKey, Resume r) {
+        storage.replace(((Resume) searchKey).getUuid(), r);
+    }
+
+    @Override
+    protected void doSave(Object searchKey, Resume r) {
+        storage.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove(((Resume) searchKey).getUuid());
+    }
+
+    @Override
+    protected void doClear() {
+        storage.clear();
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+}
